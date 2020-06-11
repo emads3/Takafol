16ad62c079de6fbe0e4984be0b551a7f40cf659d
Emad Saeed <e@emads3.com>
2020-06-12 01:47:47 +0200

    installed theme with its dependencies

---
+++ app/assets/javascripts/porto_wrapper.js
@@ -0,0 +1,25 @@
+//= require "./application"
+//= require "./porto/jquery.appear.min"
+//= require "./porto/jquery.easing.min"
+//= require "./porto/jquery-cookie.min"
+//= require './porto/bootstrap.min'
+//= require './porto/common.min'
+//= require './porto/jquery.validation.min'
+//= require './porto/jquery.easy-pie-chart.min'
+//= require './porto/jquery.gmap.min'
+//= require './porto/jquery.lazyload.min'
+//= require './porto/jquery.isotope.min'
+//= require './porto/owl.carousel.min'
+//= require './porto/jquery.magnific-popup.min'
+//= require './porto/vide.min'
+
+
+//= require './porto/theme'
+
+//= require './porto/jquery.themepunch.tools.min'
+//= require './porto/jquery.themepunch.revolution.min'
+//= require './porto/jquery.flipshow.min'
+//= require './porto/view.home'
+
+//= require './porto/custom'
+//= require './porto/theme.init'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../js-by-emad/testjs3")
+require("testjs2")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -17,5 +19,5 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+// var ReactRailsUJS = require("react_ujs");
+// ReactRailsUJS.useContext(componentRequireContext);
