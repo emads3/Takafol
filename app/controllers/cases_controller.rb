b0fea5a59a97b30814e0ccabe4f33a9e5bc19386
khaldmansour <khaldmansour93@gmail.com>
2020-06-14 16:12:31 +0200

    Modifications on Case Card and Index

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -10,62 +10,41 @@ class CasesController < ApplicationController
     @governorates = Governorate.all
     # @cases =Case.all.page params[:page]
   end
-
+  
   #Free Cases Index
   def freeindex
     @cases = Case.all.order(priority: :desc)
   end
-
+  
   #Profile
   def profile
     @cases = Case.all
     render 'profile'
   end
-
+  
   # GET /cases/1
   # GET /cases/1.json
   def show
     @case= Case.find(params["id"])
   end
-
+  
   # Add Case to Donor
   def protect
     @case = Case.find(params["id"])
-
-     if @case.donors_cases.where('state in (?)' , ["pending","cancelled","rejected"]).count < 1
+    if @case.donors_cases.where('state in (?)' , ["pending","cancelled","rejected"]).count < 1
       @case.donors << current_donor
     end
     @case.donors_cases.update(state: "pending" )
-
-    redirect_to cases_path
+    redirect_to case_path(@case)
   end
-  #Charity protects the case
-  def freeprotect
-    @case = Case.find(params["id"])
-    @case.charities << current_charity
-    @case.charities_cases.update(state: "protected" )
-    redirect_to :free_cases
-  end
-
-  #Search Button
-  def search
-    search = params[:search].present? ? params[:search] : nil
-
-    @cases =
-    if search
-      Case.where(job: search)
-    end
-
-  end
-
-
+  
   #Remove Donor's Protection
   def remove
     id = current_donor.id
     @case = Case.find(params["id"])
     @case = @case.donors_cases.where(donor_id = "#{id}").update(state: "cancelled")
-    redirect_to cases_path
-
+    redirect_to case_path(@case)
+    
   end
 
   #Charity's Approval to Donor's Request
@@ -79,52 +58,65 @@ class CasesController < ApplicationController
 
   end
 
+  #Charity protects the case
+  def freeprotect
+    @case = Case.find(params["id"])
+    @case.charities << current_charity
+    @case.charities_cases.update(state: "protected" )
+    redirect_to :free_cases
+  end
+  
+  
+  
+  
+  
+  
   # GET /cases/new
   def new
     @case = Case.new
   end
-
+  
   # GET /cases/1/edit
   def edit
   end
-
+  
   # POST /cases
   # POST /cases.json
   def create
-
+    
     #If a Charity is signed in the case will be assigned to it automatically
     if current_charity
+      @case = @current_charity.cases.create(case_params_charity)
       @case.perform_image_validation = false 
-        @case = @current_charity.cases.create(case_params_charity)
-        respond_to do |format|
-          if @case.save
-            format.html { redirect_to @case, notice: 'Case was successfully created.' }
-            format.json { render :show, status: :created, location: @case }
-            @case.charities_cases.first.update(state: "protected")
-          else
-            format.html { render :new }
-            format.json { render json: @case.errors, status: :unprocessable_entity }
-          end
+      respond_to do |format|
+        if @case.save
+          format.html { redirect_to @case, notice: 'Case was successfully created.' }
+          format.json { render :show, status: :created, location: @case }
+          @case.charities_cases.first.update(state: "protected")
+        else
+          format.html { render :new }
+          format.json { render json: @case.errors, status: :unprocessable_entity }
         end
-
-      else
-
-        @case = Case.new(case_params)
-        @case.perform_image_validation = true 
-
-        respond_to do |format|
-          if @case.save
-            format.html { redirect_to @case, notice: 'Case was successfully created.' }
-            format.json { render :show, status: :created, location: @case }
-          else
-            format.html { render :new }
-            format.json { render json: @case.errors, status: :unprocessable_entity }
-          end
+      end
+      
+    else
+      
+      @case = Case.new(case_params)
+      @case.perform_image_validation = true 
+      
+      respond_to do |format|
+        if @case.save
+          format.html { redirect_to @case, notice: 'Case was successfully created.' }
+          format.json { render :show, status: :created, location: @case }
+        else
+          format.html { render :new }
+          format.json { render json: @case.errors, status: :unprocessable_entity }
         end
-   end
-
+      end
+    end
+    
   end
-
+  
   # PATCH/PUT /cases/1
   # PATCH/PUT /cases/1.json
   def update
@@ -138,7 +130,7 @@ class CasesController < ApplicationController
       end
     end
   end
-
+  
   # DELETE /cases/1
   # DELETE /cases/1.json
   def destroy
@@ -148,26 +140,37 @@ class CasesController < ApplicationController
       format.json { head :no_content }
     end
   end
-
-
+  
+  
   def loggedCharity_cases
     id = current_charity.id
     @cases = Case.all.where(charity_id = "#{id}")
   end
 
+  #Search Button
+  def search
+    search = params[:search].present? ? params[:search] : nil
+  
+    @cases =
+    if search
+      Case.where(job: search)
+    end
+  
+  end
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_case
-      @case = Case.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def case_params
-      params.fetch(:case).permit(:name , :job ,:email , :national_id , :phone , :children_num ,:marital_status , :NID_img, :description, :city_id)
-    end
-
-    #Charity's Special Params to add Case
-    def case_params_charity
-      params.fetch(:case).permit(:name , :email , :job , :national_id , :phone ,:children_num, :marital_status, :description, :priority ,:amount_needed, :city_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_case
+    @case = Case.find(params[:id])
+  end
+  
+  # Only allow a list of trusted parameters through.
+  def case_params
+    params.fetch(:case).permit(:name , :job ,:email , :national_id , :phone , :children_num ,:marital_status , :NID_img, :description, :city_id)
+  end
+  
+  #Charity's Special Params to add Case
+  def case_params_charity
+    params.fetch(:case).permit(:name , :email , :job , :national_id , :phone ,:children_num, :marital_status, :description, :priority ,:amount_needed, :city_id)
+  end
 end
