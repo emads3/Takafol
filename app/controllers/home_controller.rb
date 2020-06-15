e32de44893c589435d57c62f5567731e4217a771
Emad Saeed <e@emads3.com>
2020-06-15 11:44:13 +0200

    select verified cases from controller - to be rendered to the view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @cases = Case.verified.joins(:donors_cases).joins(:donors)
+                 .where("donors_cases.state = 'pending' or donors_cases.state = 'cancelled'")
   end
 end
