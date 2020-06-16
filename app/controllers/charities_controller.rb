f9e790a6a49cdf39d307d0fd1cea6bf54860f5a4
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-06-17 00:00:56 +0200

    Add charities pagination

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -2,9 +2,9 @@ class CharitiesController < ApplicationController
   def index
     city_id = params['city_id']
     @charities = if city_id.nil?
-                   Charity.verified
+                   Charity.verified.page params[:page]
                  else
-                   Charity.verified.where('city_id = ?', params['city_id'])
+                   Charity.verified.where('city_id = ?', params['city_id']).page params[:page]
                  end
   end
 end
