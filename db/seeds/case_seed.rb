10e12f90d194a3603742ba04ea6e3d57c03d96ff
Emad Saeed <e@emads3.com>
2020-06-16 13:46:28 +0200

    change seeds to add some protected cases to random charities

--- db/seeds/case_seed.rb
+++ db/seeds/case_seed.rb
@@ -4,35 +4,22 @@ puts 'Running Case seeder..'
 # get charities in batches of 1000, not to get them all and cause memory problems
 Charity.take(30).each do |ch|
   (1..10).to_a.sample.times do
-    ch.cases.create(
+    _case = ch.cases.new(
       name: Faker::Name.unique.name,
       job: Faker::Job.position,
       description: Faker::Quote.famous_last_words,
-      children_num: Faker::Number.between(from: 0, to: 20),
-      marital_status: %w[single divorced].sample,
+      children_num: Faker::Number.between(from: 0, to: 8),
+      marital_status: %w[Married Divorced Single].sample,
       priority: Faker::Number.between(from: 0, to: 5),
       # address: Faker::Address.unique.full_address,
       city_id: Faker::Number.between(from: 1, to: 250),
-      phone: Faker::PhoneNumber.unique.cell_phone_with_country_code,
-      national_id: Faker::IDNumber.ssn_valid
-    ).save(validate: false)
+      phone: Faker::Base.unique.numerify('############'),
+      national_id: Faker::Base.unique.numerify('##############')
+    )
+    saving_result = _case.save(validate: false)
+    ch.charities_cases.create(case: _case, state: 'protected') if saving_result and [true, false].sample
   end
 end
 
-# insert cases without charities
-15.times do
-  Case.create(
-    name: Faker::Name.unique.name,
-    job: Faker::Job.position,
-    description: Faker::Quote.famous_last_words,
-    children_num: Faker::Number.between(from: 0, to: 20),
-    marital_status: %w[single divorced].sample,
-    priority: Faker::Number.between(from: 0, to: 5),
-    # address: Faker::Address.unique.full_address,
-    city_id: Faker::Number.between(from: 1, to: 250),
-    phone: Faker::PhoneNumber.unique.cell_phone_with_country_code,
-    national_id: Faker::IDNumber.ssn_valid
-  ).save(validate: false)
-end
 puts 'Case seeder done successfully..'
 
