c0d841e3abe983873b640edef28f9e9442d65ecc
Emad Saeed <e@emads3.com>
2020-06-16 13:39:52 +0200

    fix case validation on name, job and childs num

--- app/models/case.rb
+++ app/models/case.rb
@@ -8,12 +8,12 @@ class Case < ApplicationRecord
     mount_uploader :NID_img, NidUploader
     attr_accessor :perform_image_validation
     paginates_per 3
-    validates :name , presence: true ,  format: { with: /\A[a-z\s]+\Z/i, message:  "Name must only contain letters." }
-    validates :job , presence: true ,  format: { with: /\A[a-z\s]+\Z/i, message:  "Job must only contain letters." }
+    validates :name , presence: true
+    validates :job , presence: true
     validates :email, email_format: { message: 'Invalid email format' } , allow_nil: true, uniqueness: false, presence: false
     validates :national_id, format: { with: /\A[+-]?\d+\z/ , message:  "National Id must only contain Numbers." } , uniqueness: true , length: { is: 14  }
     validates :NID_img , presence:  {message:  "you have to upload a National Id image" } , if: :perform_image_validation
-    validates :children_num, :inclusion => { :in => 0..8 }
+    validates :children_num, :inclusion => { :in => 0..20 }
     validates :marital_status , presence: true, inclusion: { in: %w(Married Divorced Single), message: 'marital status is not defined'}
     validates :phone, format: { with: /\A[+-]?\d+\z/ , message:  "Phone must only contain Numbers." } , uniqueness: false
     validates :priority, :inclusion => { :in => 1..5 } , allow_nil: true
